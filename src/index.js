298b66d60af53ba9344b03db7154e8880ca5e842
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-10-17 11:20:20 +0200

    Add fade in to .container

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles.css";
 import "./normalize.css";
 
+//.container fades in after 200ms
+document.addEventListener("DOMContentLoaded", function() {
+const container = document.querySelector(".container");
+setTimeout(function() {
+container.classList.toggle("hide");
+container.classList.toggle("show");
+}, 200);
+});
